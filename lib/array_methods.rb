0e70fcf251444ecbc825df134a8a1fba9eb6ac71
megan <meggsnbacon777@gmail.com>
2020-07-04 01:23:16 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,7 @@ def using_include(array,element)
 using_include(array,element).include?("wow")
 using_include(array,element).include?("Tardis")
 end
-using_include(array,element)
+
 
 def using_sort(array)
 
